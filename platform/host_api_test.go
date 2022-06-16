9f6af59e44ecdc8b0fce52260df9d2474213bd13
Szubie <benszubert@gmail.com>
2022-06-16 13:03:04 +0100

    Cleanup after unit deployment test. Add functions to excercise host API functions that print.

--- platform/host_api_test.go
+++ platform/host_api_test.go
@@ -90,6 +90,13 @@ func Test_InitUnit(t *testing.T) {
 	bravefile.PlatformService.Resources.CPU = "1"
 	bravefile.PlatformService.Resources.RAM = "1GB"
 
+	bravefile.PlatformService.Postdeploy.Run = []shared.RunCommand{
+		{
+			Command: "echo",
+			Args:    []string{"Hello World"},
+		},
+	}
+
 	err := host.BuildImage(&bravefile)
 	if err != nil {
 		t.Error("host.BuildImage: ", err)
@@ -100,8 +107,58 @@ func Test_InitUnit(t *testing.T) {
 		t.Error("host.InitUnit: ", err)
 	}
 
+	err = host.Postdeploy(&bravefile)
+	if err != nil {
+		t.Error("host.Postdeploy: ", err)
+	}
+
 	err = host.DeleteLocalImage("alpine-test-1.0")
 	if err != nil {
 		t.Error("host.DeleteImageByName: ", err)
+	}
+
+	err = host.StopUnit("alpine-test", backend)
+	if err != nil {
+		t.Error("host.StopUnit: ", err)
+	}
+
+	err = host.StartUnit("alpine-test", backend)
+	if err != nil {
+		t.Error("host.StartUnit: ", err)
+	}
+
+	err = host.DeleteUnit("alpine-test")
+	if err != nil {
+		t.Error("host.DeleteUnit: ", err)
+	}
+}
+
+func Test_ListLocalImages(t *testing.T) {
+	host := *NewBraveHost()
+	backend := NewLxd(host.Settings)
+
+	err := host.HostInfo(backend, false)
+	if err != nil {
+		t.Error("host.HostInfo: ", err)
+	}
+
+	err = host.ListLocalImages()
+	if err != nil {
+		t.Error("host.ListLocalImages: ", err)
+	}
+}
+
+func Test_ListUnits(t *testing.T) {
+	host := *NewBraveHost()
+	backend := NewLxd(host.Settings)
+
+	err := host.HostInfo(backend, false)
+	if err != nil {
+		t.Error("host.HostInfo: ", err)
+	}
+
+	err = host.ListUnits(backend)
+	if err != nil {
+		t.Error("host.ListLocalImages: ", err)
 	}
 }
