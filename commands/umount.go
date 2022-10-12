b1f3e95090d1e97829bc6f130d7463e50b02f79a
Szubie <benszubert@gmail.com>
2022-10-12 21:32:24 +0100

    Update usage messages for CLI `umount` command to reflect that we are using paths instead of devices now

--- commands/umount.go
+++ commands/umount.go
@@ -8,8 +8,8 @@ import (
 )
 
 var umountDir = &cobra.Command{
-	Use:   "umount UNIT:<disk>",
-	Short: "Unmount <disk> from UNIT",
+	Use:   "umount UNIT:<path>",
+	Short: "Unmount device mounted on <path> from UNIT",
 	Long:  ``,
 	Run:   umount,
 }
@@ -17,14 +17,14 @@ var umountDir = &cobra.Command{
 func umount(cmd *cobra.Command, args []string) {
 	checkBackend()
 	if len(args) == 0 {
-		log.Fatal("missing UNIT:<disk>")
+		log.Fatal("missing UNIT:<path>")
 		return
 	}
 
 	for _, arg := range args {
 		remote := strings.SplitN(arg, ":", -1)
 		if len(remote) == 1 {
-			log.Fatal("target directory should be specified as UNIT:<disk>")
+			log.Fatal("target directory should be specified as UNIT:<path>")
 		}
 
 		err := host.UmountShare(remote[0], remote[1])
