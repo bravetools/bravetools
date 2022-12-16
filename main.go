02e379535505cdf5dca8618c2276048714f179e9
Szubie <benszubert@gmail.com>
2022-12-16 01:32:26 +0000

    Embed version file into Go executable to ensure version can always be retrieved at runtime regardless of paths

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	_ "embed"
+
 	"log"
 
 	"github.com/bravetools/bravetools/commands"
+	"github.com/bravetools/bravetools/shared"
 )
+
+//go:embed VERSION
+var versionString string
 
 type cmdGlobal struct {
 	configPath string
 }
 
 func main() {
+	shared.Version = versionString
 
 	err := commands.BravetoolsCmd.Execute()
 	if err != nil && err.Error() != "" {
