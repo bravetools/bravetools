a077ac84630147970aa67f39eae714e25c72f46b
Szubie <benszubert@gmail.com>
2022-12-21 16:28:43 +0000

    Add config files used for building debian package

---
+++ build/scripts/postremove.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+echo ""
+echo "Remove ~/.bravetools and any remaining user LXD resources (profile, disk, bridge). Note the bridge is shared with other users."
+echo ""
--- xcompile.sh
+++ xcompile.sh
@@ -2,4 +2,4 @@
 set -e
 # you may need to go install github.com/mitchellh/gox@v1.0.1 first
 
-CGO_ENABLED=1 gox -ldflags "${LDFLAGS}" -output="bin/brave_{{.OS}}_{{.Arch}}" --osarch="darwin/amd64 darwin/arm64 linux/amd64 linux/arm linux/arm64 windows/386 windows/amd64"
+CGO_ENABLED=0 gox -ldflags "-s -w" -output="bin/brave_{{.OS}}_{{.Arch}}" --osarch="darwin/amd64 darwin/arm64 linux/amd64 linux/arm linux/arm64 windows/386 windows/amd64"
