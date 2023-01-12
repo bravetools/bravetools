9b1b11001136f61f9629d39ab29bb36336be879d
Szubie <benszubert@gmail.com>
2023-01-12 15:50:10 +0000

    Add autocompletion to the export command

--- commands/export.go
+++ commands/export.go
@@ -12,6 +12,19 @@ var braveExportImage = &cobra.Command{
 	Short: "Export bravetools image from local image store as LXD tarball",
 	Long:  ``,
 	Run:   exportImage,
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		if len(args) != 0 {
+			return nil, cobra.ShellCompDirectiveNoFileComp
+		}
+		return func() []string {
+			var imageNames []string
+			images, _ := platform.GetLocalImages()
+			for _, image := range images {
+				imageNames = append(imageNames, image.String())
+			}
+			return imageNames
+		}(), cobra.ShellCompDirectiveNoFileComp
+	},
 }
 
 var imageExportDir string
